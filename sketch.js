b1097facc5194371d22e2ccf98209ec8b29343a6
kaushal197 <kaushal197@gmail.com>
2017-09-27 23:46:27 -0400

    Added bulk of solution, check README for TODO

--- sketch.js
+++ sketch.js
@@ -1,78 +1,66 @@
+var n = 7;
+
 var points = [
-    /*[0, 20],
-    [40, 0],
-    [40, 20],
-    [70, 50],
-    [50, 70],
-    [30, 50],
-    [0, 50],*/
+    [0, 0],
+    [50, 0],
+    [50, 50],
+    [40, 50],
+    [25, 20],
+    [10, 50],
+    [0, 50]
 ];
 
 var lines = [];
+var maxLine = 0;
 
 function setup(){
     createCanvas(640, 480);
-    frameRate(1/2);
+    noLoop();
 }
 
 function draw(){
-    clear();
-    n = 13;
-    maxLength = 0;
-    maxLine = [0, 0, 0, 0];
-    for(i=0;i<n;i++){
-        
-        //Create point and push into array
-        var v = [int(random(20, 600)), int(random(20, 440))];
-        points.push(v);
-        
-        //Need at least two points to obtain a linear function
-        if(points.length >= 2){
-            //Retrieve points
-            var x1 = points[i-1][0];
-            var y1 = points[i-1][1];
-            var x2 = v[0];
-            var y2 = v[1];
 
-            //Calculate slope(m) and y-intercept(b)
-            var m = (y2 - y1)/(x2 - x1);
-            var b = y1 - (m * x1)
+    fill('#ccc'); //Add background color to polygon
+    beginShape(); //Begin polygon shape
+    for(i=0; i<n; i++){ //Add vertices to shape from points array
+        vertex(points[i][0], points[i][1]);
 
-            lines.push([m, b]);
+        if(i >= 1){
+            x1 = points[i-1][0];
+            y1 = points[i-1][1];
+            x2 = points[i][0];
+            y2 = points[i][1];
+
+            lines.push({x : {a : x1, b : x2-x1}, y : {a : y1, b : y2-y1}});
         }
     }
 
-    fill('#c0c0c0');
-    stroke('#303030');
-    beginShape();
+    x1 = points[n-1][0];
+    y1 = points[n-1][1];
+    x2 = points[0][0];
+    y2 = points[0][1];
 
-    for(i=0;i<points.length;i++){
-        v = points[i];
-        vertex(v[0], v[1]);
-    }
+    lines.push({x : {a : x1, b : x2-x1}, y : {a : y1, b : y2-y1}});
 
-    endShape(CLOSE);
+    translate(width/2, height/2);
+    scale(3.0);
+    endShape(CLOSE); //End polygon and close last line
+    console.log(lines); //Checking lines array
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(j != i){
-                x1 = points[i][0];
-                x2 = points[j][0];
-                y1 = points[i][1];
-                y2 = points[j][1];
-                
-                var dist = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
-                
-                if(dist > maxLength){
-                    maxLength = dist;
-                    maxLine = [x1, y1, x2, y2];
-                }
-            }
+    //Check all lines for max length using the points array
+    //Initiate a new line and cross reference with the lines array for intersections
+    for(i=0; i<n-1; i++){
+        //Initialize the first vertex coordinates
+        x1 = points[i][0];
+        y1 = points[i][1];
+
+        for(j=i+1; j<n; j++){
+            //Initialize the second vertex coordinates
+            x2 = points[j][0];
+            y2 = points[j][1];
+
+            //Create temporary line object for current set of vertices
+            tLine = {x : {a : x1, b : x2-x1}, y : {a : y1, b : y2-y1}};
         }
     }
-
-    stroke('red');
-    line(maxLine[0], maxLine[1], maxLine[2], maxLine[3]);
-    console.log('Length: ' + maxLength);
-    points = [];
 }
